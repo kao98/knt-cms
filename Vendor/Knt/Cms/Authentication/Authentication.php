ee92c7fa4499dd7217491dbbf9dfdff57526a7bc
aurelien <aurelien@knt-st-xubuntu>
2013-03-17 14:53:27 +0100

    Windows => Linux

--- Vendor/Knt/Cms/Authentication/Authentication.php
+++ Vendor/Knt/Cms/Authentication/Authentication.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * knt-cms: another Content Management System (http://www.kaonet-fr.net/cms)
  * 
  * Licensed under The MIT License
@@ -11,23 +11,14 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-namespace Knt\Framework\Exception;
-
 /**
- * KntFrameworkException.php
- * Creation date: 07 feb. 2013
+ * Authentication.php
+ * Creation date: 27 nov. 2012
  * 
- * KNT Framework Exception class.
- * Exception thrown by the framework itself. 
- * May be used as base class for all exception thrown by the framework.
- * 
- * Version 1.0: Initial version
+ * Description of Authentication
  *
- * @package Knt\Framework\Exception
- * @version 1.0
  * @author Aurélien Reeves (Kao ..98)
  */
-class KntFrameworkException extends \Exception
-{
-
+class Authentication {
+    
 }
